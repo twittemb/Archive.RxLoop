f7b27b32d988ab7678c440c07147c970d96a534b
Thibault Wittemberg <twittemberg@genetec.com>
2019-06-23 17:55:45 -0400

    loop: add interpretation Queue

--- RxLoop/Loop.swift
+++ RxLoop/Loop.swift
@@ -17,20 +17,33 @@ public class LoopRuntime<State> {
     private var loop: Loop<State>
     private let state = PublishRelay<State>()
     private let interpreter: StateInterpreter<State>
+    private let interpretationScheduler: SchedulerType
     
-    init(with loop: @escaping Loop<State>, interpreter: @escaping StateInterpreter<State>) {
+    fileprivate init(with loop: @escaping Loop<State>, interpreter: @escaping StateInterpreter<State>, interpretationScheduler: SchedulerType) {
         self.loop = loop
         self.interpreter = interpreter
+        self.interpretationScheduler = interpretationScheduler
     }
 
     public func start (with initialState: State) -> Disposable {
         let initialStateObservable = state.startWith(initialState)
-        return self.loop(initialStateObservable).observeOn(MainScheduler.instance).do(onNext: self.interpreter).bind(to: self.state)
+        return self.loop(initialStateObservable).observeOn(interpretationScheduler).do(onNext: self.interpreter).bind(to: self.state)
     }
 
     public func start<ObservableTypeType: ObservableType>(with initialState: State, when trigger: ObservableTypeType) -> Disposable {
         let initialStateObservable = state.startWith(initialState)
-        return trigger.take(1).flatMap { _ -> Observable<State> in return self.loop(initialStateObservable) }.bind(to: self.state)
+        return trigger.take(1)
+            .observeOn(interpretationScheduler)
+            .do(onNext: { _ in
+                self.interpreter(initialState)
+            })
+            .flatMap { _ -> Observable<State> in
+                return self
+                    .loop(initialStateObservable)
+                    .observeOn(self.interpretationScheduler)
+                    .do(onNext: self.interpreter)
+            }
+            .bind(to: self.state)
     }
 
     public func start(with initialState: State, after dueTime: RxTimeInterval, on scheduler: SchedulerType = MainScheduler.instance) -> Disposable {
@@ -43,14 +56,15 @@ public class LoopRuntime<State> {
             return self.loop(state).takeUntil(trigger)
         }
 
-        return LoopRuntime<State>(with: untilLoop, interpreter: self.interpreter)
+        return LoopRuntime<State>(with: untilLoop, interpreter: self.interpreter, interpretationScheduler: self.interpretationScheduler)
     }
 }
 
 
 public func loop<Mutation, State> (mutationEmitter: @escaping MutationEmitter<Mutation>,
                                    reducer: @escaping Reducer<State, Mutation>,
-                                   interpreter: @escaping StateInterpreter<State>) -> LoopRuntime<State> {
+                                   interpreter: @escaping StateInterpreter<State>,
+                                   interpretationScheduler: SchedulerType = MainScheduler.instance) -> LoopRuntime<State> {
 
     func tuplize(states: Observable<State>, mutations: Observable<Mutation>) -> Observable<(State, Mutation)> {
         return mutations.withLatestFrom(states) { ($1, $0) }
@@ -62,12 +76,8 @@ public func loop<Mutation, State> (mutationEmitter: @escaping MutationEmitter<Mu
         }
     }
 
-    //        let state = PublishRelay<StateType>()
-    //        let initialStateObservable = state.startWith(initialState)
     let stateBinderToReducer = composeAndAggregate(f1: mutationEmitter, f2: tuplize)
     let observableReducer = makeObservable(f: reducer)
-    let stateBinderToReducedState = compose1(f1: stateBinderToReducer, f2: observableReducer) // -> suite de fibonacci: Etat N = Etat 0 + Etat N-1
-    // should return the stateBinderToReducedState as a result to be able to feed the stateInterpreter when starting the LoopRuntime
-//    let loop = stateBinderToReducedState(initialStateObservable).observeOn(MainScheduler.instance).do(onNext: stateInterpreter)
-    return LoopRuntime(with: stateBinderToReducedState, interpreter: interpreter)
+    let stateBinderToReducedState = compose1(f1: stateBinderToReducer, f2: observableReducer)
+    return LoopRuntime(with: stateBinderToReducedState, interpreter: interpreter, interpretationScheduler: interpretationScheduler)
 }
