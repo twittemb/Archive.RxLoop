fd609e1e601f6d413d8e6b2ee0be2058f7c8233d
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-07-21 17:58:39 -0400

    loop: apply builder pattern

--- RxLoop/Functional.swift
+++ RxLoop/Functional.swift
@@ -1,140 +1,140 @@
+////
+////  Functional.swift
+////  RxLoop
+////
+////  Created by Thibault Wittemberg on 2019-03-07.
+////  Copyright © 2019 WarpFactor. All rights reserved.
+////
 //
-//  Functional.swift
-//  RxLoop
+//import RxSwift
 //
-//  Created by Thibault Wittemberg on 2019-03-07.
-//  Copyright © 2019 WarpFactor. All rights reserved.
+///// Compose 2 functions into a third one having the input type
+///// of the first function and the output type of the second one.
+///// 
+///// example:
+///// ```
+///// func square (a: Int) -> Int { return a*a }
+///// func stringify (a: Int) -> String { return "\(a)" }
+/////
+///// let composedFunc = compose(f1: square, f2: stringify)
+/////
+///// will be a function of type: (Int) -> String
+///// and composedFunc(3) will result in the String "9"
+///// ```
+/////
+///// - Parameters:
+/////     - f1: The first function which output type has to be the same as the second function input type
+/////     - f2: The second function
+///// - Returns: A third function having the input type of the first function and the output type of the second one
+//public func compose1<A, B, C> (f1: @escaping (A) -> B, f2: @escaping (B) -> C) -> (A) -> C {
+//    return { a in f2(f1(a)) }
+//}
 //
-
-import RxSwift
-
-/// Compose 2 functions into a third one having the input type
-/// of the first function and the output type of the second one.
-/// 
-/// example:
-/// ```
-/// func square (a: Int) -> Int { return a*a }
-/// func stringify (a: Int) -> String { return "\(a)" }
-///
-/// let composedFunc = compose(f1: square, f2: stringify)
-///
-/// will be a function of type: (Int) -> String
-/// and composedFunc(3) will result in the String "9"
-/// ```
-///
-/// - Parameters:
-///     - f1: The first function which output type has to be the same as the second function input type
-///     - f2: The second function
-/// - Returns: A third function having the input type of the first function and the output type of the second one
-public func compose1<A, B, C> (f1: @escaping (A) -> B, f2: @escaping (B) -> C) -> (A) -> C {
-    return { a in f2(f1(a)) }
-}
-
-public func compose2<A, B> (f1: @escaping () -> A, f2: @escaping (A) -> B) -> () -> B {
-    return { f2(f1()) }
-}
-
-/// Compose 2 functions into a third one having 2 input types: the input type of the first function, the second input type of the second function
-/// The output type of the composed function it the same as the output
-/// type of the second function.
-///
-/// example:
-/// ```
-/// func divideBy2 (a: Int) -> Double { return a/2 }
-/// func stringify (a: Double, b: Bool) -> String { return "\(a), \(b)" }
-///
-/// let composedFunc = compose(f1: divideBy2, f2: stringify)
-///
-/// will be a function of type: (Int, Bool) -> String
-/// and composedFunc(3, true) will result in the String "1.5, true"
-/// ```
-///
-/// The idea is to `agregate` the two distinct parameters of the 2 functions,
-/// by masking the type they have in common: the output of the first function
-/// and the first input of the second one.
-///
-/// - Parameters:
-///     - f1: The first function which output type has to be the same as the second function first input type
-///     - f2: The second function
-/// - Returns: A third function having the input types of the first and second functions and the output type of the second one
-func composeAndAggregate<A, B, C> (f1: @escaping () -> A, f2: @escaping (B, A) -> C) -> (B) -> C {
-    return { (b) in f2(b, f1()) }
-}
-
-func composeAndAggregate2<A, B, C, D> (f1: @escaping (A) -> B, f2: @escaping (B, C) -> D) -> (A, C) -> D {
-    return { (a, c) in f2(f1(a), c) }
-}
-
-/// Compose 2 functions into a third one having the two input types
-/// of the first function and the output type of the second one.
-///
-/// example:
-/// ```
-/// func multiply (a: Int, b: Double) -> Double { return a*b }
-/// func stringify (a: Double) -> String { return "\(a)" }
-///
-/// let composedFunc = compose(f1: multiply, f2: stringify)
-///
-/// will be a function of type: (Int, Double) -> String
-/// and composedFunc(3, 4.0) will result in the String "12.0"
-/// ```
-///
-/// - Parameters:
-///     - f1: The first function which output type has to be the same as the second function input type
-///     - f2: The second function
-/// - Returns: A function having the two input types of the first function and the output type of the second one.
-func composeWithTwoParameters<A, B, C, D> (f1: @escaping (A, B) -> C, f2: @escaping (C) -> D) -> (A, B) -> D {
-    return { (a, b) in f2(f1(a, b)) }
-}
-
-/// Transform an array of functions having the same signature into a function having the input type
-/// of the individual functions and having an output type being an array of output types of the individual function.
-///
-/// example:
-/// ```
-/// func square (a: Int) -> Int { return a*a }
-/// func addOne (a: Int) -> Int { return a+1 }
-/// func multiplyByTwo (a: Int) -> Int { return a*2 }
-///
-///
-/// let composedFunc = flatten(funcs: [square, addOne, multiplyByTwo]])
-///
-/// will be a function of type: (Int) -> [Int]
-/// and composedFunc(2) will result in the Array [4, 3, 4]
-/// ```
-///
-/// - Parameter funcs: The array of functions to flatten
-/// - Returns: A function applying each individual function on the input
-func flatten<A, B> (funcs: [(A) -> B]) -> (A) -> [B] {
-    return { a in funcs.map { $0(a) } }
-}
-
-func flatten<A> (funcs: [() -> A]) -> () -> [A] {
-    return { funcs.map { $0() } }
-}
-
-public func merge<A> (_ funcs: (() -> A)...) -> () -> A where A: ObservableType {
-
-    func observableMerge(inputs: [A]) -> A {
-        return Observable<A.Element>.merge(inputs.map { $0.asObservable() }) as! A
-    }
-
-    let flatFuncs = flatten(funcs: funcs)
-    return compose2(f1: flatFuncs, f2: observableMerge)
-}
-
-public func merge<A, B> (_ funcs: ((A) -> B)...) -> (A) -> B where A: ObservableType, B: ObservableType {
-
-    func observableMerge(inputs: [B]) -> B {
-        return Observable<B.Element>.merge(inputs.map { $0.asObservable() }) as! B
-    }
-
-    let flatFuncs = flatten(funcs: funcs)
-    return compose1(f1: flatFuncs, f2: observableMerge)
-}
-
-public func concat<A> (_ funcs: ((A) -> Void)...) -> (A) -> Void {
-    return { (a:A) in
-        funcs.forEach { $0(a) }
-    }
-}
+//public func compose2<A, B> (f1: @escaping () -> A, f2: @escaping (A) -> B) -> () -> B {
+//    return { f2(f1()) }
+//}
+//
+///// Compose 2 functions into a third one having 2 input types: the input type of the first function, the second input type of the second function
+///// The output type of the composed function it the same as the output
+///// type of the second function.
+/////
+///// example:
+///// ```
+///// func divideBy2 (a: Int) -> Double { return a/2 }
+///// func stringify (a: Double, b: Bool) -> String { return "\(a), \(b)" }
+/////
+///// let composedFunc = compose(f1: divideBy2, f2: stringify)
+/////
+///// will be a function of type: (Int, Bool) -> String
+///// and composedFunc(3, true) will result in the String "1.5, true"
+///// ```
+/////
+///// The idea is to `agregate` the two distinct parameters of the 2 functions,
+///// by masking the type they have in common: the output of the first function
+///// and the first input of the second one.
+/////
+///// - Parameters:
+/////     - f1: The first function which output type has to be the same as the second function first input type
+/////     - f2: The second function
+///// - Returns: A third function having the input types of the first and second functions and the output type of the second one
+//func composeAndAggregate<A, B, C> (f1: @escaping () -> A, f2: @escaping (B, A) -> C) -> (B) -> C {
+//    return { (b) in f2(b, f1()) }
+//}
+//
+//func composeAndAggregate2<A, B, C, D> (f1: @escaping (A) -> B, f2: @escaping (B, C) -> D) -> (A, C) -> D {
+//    return { (a, c) in f2(f1(a), c) }
+//}
+//
+///// Compose 2 functions into a third one having the two input types
+///// of the first function and the output type of the second one.
+/////
+///// example:
+///// ```
+///// func multiply (a: Int, b: Double) -> Double { return a*b }
+///// func stringify (a: Double) -> String { return "\(a)" }
+/////
+///// let composedFunc = compose(f1: multiply, f2: stringify)
+/////
+///// will be a function of type: (Int, Double) -> String
+///// and composedFunc(3, 4.0) will result in the String "12.0"
+///// ```
+/////
+///// - Parameters:
+/////     - f1: The first function which output type has to be the same as the second function input type
+/////     - f2: The second function
+///// - Returns: A function having the two input types of the first function and the output type of the second one.
+//func composeWithTwoParameters<A, B, C, D> (f1: @escaping (A, B) -> C, f2: @escaping (C) -> D) -> (A, B) -> D {
+//    return { (a, b) in f2(f1(a, b)) }
+//}
+//
+///// Transform an array of functions having the same signature into a function having the input type
+///// of the individual functions and having an output type being an array of output types of the individual function.
+/////
+///// example:
+///// ```
+///// func square (a: Int) -> Int { return a*a }
+///// func addOne (a: Int) -> Int { return a+1 }
+///// func multiplyByTwo (a: Int) -> Int { return a*2 }
+/////
+/////
+///// let composedFunc = flatten(funcs: [square, addOne, multiplyByTwo]])
+/////
+///// will be a function of type: (Int) -> [Int]
+///// and composedFunc(2) will result in the Array [4, 3, 4]
+///// ```
+/////
+///// - Parameter funcs: The array of functions to flatten
+///// - Returns: A function applying each individual function on the input
+//func flatten<A, B> (funcs: [(A) -> B]) -> (A) -> [B] {
+//    return { a in funcs.map { $0(a) } }
+//}
+//
+//func flatten<A> (funcs: [() -> A]) -> () -> [A] {
+//    return { funcs.map { $0() } }
+//}
+//
+//public func merge<A> (_ funcs: (() -> A)...) -> () -> A where A: ObservableType {
+//
+//    func observableMerge(inputs: [A]) -> A {
+//        return Observable<A.Element>.merge(inputs.map { $0.asObservable() }) as! A
+//    }
+//
+//    let flatFuncs = flatten(funcs: funcs)
+//    return compose2(f1: flatFuncs, f2: observableMerge)
+//}
+//
+//public func merge<A, B> (_ funcs: ((A) -> B)...) -> (A) -> B where A: ObservableType, B: ObservableType {
+//
+//    func observableMerge(inputs: [B]) -> B {
+//        return Observable<B.Element>.merge(inputs.map { $0.asObservable() }) as! B
+//    }
+//
+//    let flatFuncs = flatten(funcs: funcs)
+//    return compose1(f1: flatFuncs, f2: observableMerge)
+//}
+//
+//public func concat<A> (_ funcs: ((A) -> Void)...) -> (A) -> Void {
+//    return { (a:A) in
+//        funcs.forEach { $0(a) }
+//    }
+//}
--- RxLoop/LoopCombine.swift
+++ RxLoop/LoopCombine.swift
@@ -7,19 +7,106 @@
 //
 import Combine
 
-public typealias MutationEmitterCombine<Mutation> = () -> AnyPublisher<Mutation, Never>
-public typealias ReducerCombine<State, Mutation> = (State, Mutation) -> State
-public typealias StateInterpreterCombine<State> = (State) -> Void
+public typealias MutationEmitter<Mutation> = () -> AnyPublisher<Mutation, Never>
+public typealias Reducer<State, Mutation> = (State, Mutation) -> State
+public typealias StateInterpreter<State> = (State) -> Void
 
-public class LoopRuntimeCombine<Mutation, State> {
-    private let mutationEmitter: MutationEmitterCombine<Mutation>
-    private let reducer: ReducerCombine<State, Mutation>
-    private let interpreter: StateInterpreterCombine<State>
+public class LoopBuilder {
+    public static func mutates<Mutation> (with emitter: @escaping MutationEmitter<Mutation>) -> MutationEmitterBuilder<Mutation> {
+        return MutationEmitterBuilder<Mutation>(emitter: emitter)
+    }
+}
+
+public class MutationEmitterBuilder<Mutation> {
+    private let mutationEmitter: MutationEmitter<Mutation>
+
+    private static func composeEmitters<A, B> (f1: @escaping () -> A, f2: @escaping (A) -> B) -> () -> B {
+        return { f2(f1()) }
+    }
+
+    private static func flatten<A> (funcs: [() -> A]) -> () -> [A] {
+        return { funcs.map { $0() } }
+    }
+
+    private static func mergeEmitter<A> (_ funcs: (() -> A)...) -> () -> A where A: Publisher {
+
+        func observableMerge(inputs: [A]) -> A {
+            return Publishers.MergeMany(inputs).eraseToAnyPublisher() as! A
+        }
+
+        let flatFuncs = MutationEmitterBuilder.flatten(funcs: funcs)
+        return MutationEmitterBuilder.composeEmitters(f1: flatFuncs, f2: observableMerge)
+    }
+
+    fileprivate init (emitter: @escaping MutationEmitter<Mutation>) {
+        self.mutationEmitter = emitter
+    }
+
+    public func compose<NextMutation> (withNextMutationEmitter nextEmitter: @escaping (AnyPublisher<Mutation, Never>) -> AnyPublisher<NextMutation, Never>) -> MutationEmitterBuilder<NextMutation> {
+        let emitter = MutationEmitterBuilder.composeEmitters(f1: self.mutationEmitter, f2: nextEmitter)
+        return MutationEmitterBuilder<NextMutation>(emitter: emitter)
+    }
+
+    public func merge (withConcurrentMutationEmitter concurrentEmitter: @escaping MutationEmitter<Mutation>) -> MutationEmitterBuilder<Mutation> {
+        let emitter = MutationEmitterBuilder.mergeEmitter(self.mutationEmitter, concurrentEmitter)
+        return MutationEmitterBuilder<Mutation>(emitter: emitter)
+    }
+
+    public func reduces<State> (with reducer: @escaping Reducer<State, Mutation>) -> ReducerBuilder<Mutation, State> {
+        return ReducerBuilder<Mutation, State>(emitter: self.mutationEmitter, reducer: reducer)
+    }
+}
+
+public class ReducerBuilder<Mutation, State> {
+    private let mutationEmitter: MutationEmitter<Mutation>
+    private let reducer: Reducer<State, Mutation>
+
+    fileprivate init (emitter: @escaping MutationEmitter<Mutation>, reducer: @escaping Reducer<State, Mutation>) {
+        self.mutationEmitter = emitter
+        self.reducer = reducer
+    }
+
+    public func interprets (with stateInterpreter: @escaping StateInterpreter<State>) -> StateInterpreterBuilder<Mutation, State> {
+        return StateInterpreterBuilder<Mutation, State>(emitter: self.mutationEmitter, reducer: self.reducer, stateInterpreter: stateInterpreter)
+    }
+}
+
+public class StateInterpreterBuilder<Mutation, State> {
+    private let mutationEmitter: MutationEmitter<Mutation>
+    private let reducer: Reducer<State, Mutation>
+    private let stateInterpreter: StateInterpreter<State>
+
+    private static func concatInterpreters<A> (_ funcs: ((A) -> Void)...) -> (A) -> Void {
+        return { (a:A) in
+            funcs.forEach { $0(a) }
+        }
+    }
+
+    fileprivate init (emitter: @escaping MutationEmitter<Mutation>, reducer: @escaping Reducer<State, Mutation>, stateInterpreter: @escaping StateInterpreter<State>) {
+        self.mutationEmitter = emitter
+        self.reducer = reducer
+        self.stateInterpreter = stateInterpreter
+    }
+
+    public func concat (withNextStateInterpreter nextStateInterpreter: @escaping StateInterpreter<State>) -> StateInterpreterBuilder<Mutation, State> {
+        let stateInterpreters = StateInterpreterBuilder.concatInterpreters(self.stateInterpreter, nextStateInterpreter)
+        return StateInterpreterBuilder<Mutation, State>(emitter: self.mutationEmitter, reducer: self.reducer, stateInterpreter: stateInterpreters)
+    }
+
+    public func interpret (on queue: DispatchQueue) -> LoopRuntime<Mutation, State> {
+        return LoopRuntime(with: self.mutationEmitter, reducer: self.reducer, interpreter: self.stateInterpreter, interpretationQueue: queue)
+    }
+}
+
+public class LoopRuntime<Mutation, State> {
+    private let mutationEmitter: MutationEmitter<Mutation>
+    private let reducer: Reducer<State, Mutation>
+    private let interpreter: StateInterpreter<State>
     private let interpretationQueue: DispatchQueue
     
-    init (with mutationEmitter: @escaping MutationEmitterCombine<Mutation>,
-          reducer: @escaping ReducerCombine<State, Mutation>,
-          interpreter: @escaping StateInterpreterCombine<State>,
+    init (with mutationEmitter: @escaping MutationEmitter<Mutation>,
+          reducer: @escaping Reducer<State, Mutation>,
+          interpreter: @escaping StateInterpreter<State>,
           interpretationQueue: DispatchQueue = .main) {
         self.mutationEmitter = mutationEmitter
         self.reducer = reducer
@@ -46,28 +133,28 @@ public class LoopRuntimeCombine<Mutation, State> {
             .handleEvents(receiveOutput: { _ in self.interpreter(initialState) })
             .flatMap { _ -> AnyPublisher<State, Never> in
                 return self.startStatePublisher(with: initialState)
-            }
-            .subscribe(PassthroughSubject<State, Never>())
+        }
+        .subscribe(PassthroughSubject<State, Never>())
     }
     
-    public func take<PublisherType: Publisher> (until trigger: PublisherType) -> LoopRuntimeCombine<Mutation, State> where PublisherType.Failure == Never {
+    public func take<PublisherType: Publisher> (until trigger: PublisherType) -> LoopRuntime<Mutation, State> where PublisherType.Failure == Never {
         func untilLoop() -> AnyPublisher<Mutation, Never> {
             return self.mutationEmitter().prefix(untilOutputFrom: trigger).eraseToAnyPublisher()
         }
         
-        return LoopRuntimeCombine<Mutation, State>(with: untilLoop,
-                                                   reducer: self.reducer,
-                                                   interpreter: self.interpreter,
-                                                   interpretationQueue: self.interpretationQueue)
+        return LoopRuntime<Mutation, State>(with: untilLoop,
+                                            reducer: self.reducer,
+                                            interpreter: self.interpreter,
+                                            interpretationQueue: self.interpretationQueue)
     }
 }
 
-public func loopCombine<Mutation, State> (mutationEmitter: @escaping MutationEmitterCombine<Mutation>,
-                                          reducer: @escaping ReducerCombine<State, Mutation>,
-                                          interpreter: @escaping StateInterpreterCombine<State>,
-                                          interpretationQueue: DispatchQueue = .main) -> LoopRuntimeCombine<Mutation, State> {
-    return LoopRuntimeCombine(with: mutationEmitter,
-                              reducer: reducer,
-                              interpreter: interpreter,
-                              interpretationQueue: interpretationQueue)
-}
+//public func loopCombine<Mutation, State> (mutationEmitter: @escaping MutationEmitterCombine<Mutation>,
+//                                          reducer: @escaping ReducerCombine<State, Mutation>,
+//                                          interpreter: @escaping StateInterpreterCombine<State>,
+//                                          interpretationQueue: DispatchQueue = .main) -> LoopRuntime<Mutation, State> {
+//    return LoopRuntime(with: mutationEmitter,
+//                       reducer: reducer,
+//                       interpreter: interpreter,
+//                       interpretationQueue: interpretationQueue)
+//}
